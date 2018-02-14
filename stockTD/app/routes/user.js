20ad087c77c4fe59b86e2a355ed2d07bf1d1343d
jeremy_puertolas <jeremy.puertolas@gmail.com>
2018-02-14 16:36:55 +0100

    td2 partie 1 en cours

---
+++ stockTD/app/routes/user.js
@@ -0,0 +1,81 @@
+import Route from '@ember/routing/route';
+import EmberObject, { computed } from '@ember/object';
+
+const Listes=EmberObject.extend({
+  includedItems:[],
+  dispoItemsIds_:[],
+  includedItemsIds_:[],
+  dispoItems_:Ember.computed('dispoItemsIds_.[]',function(){
+    return this.get('dispoItemsIds_').map((id)=>{
+      return this.get('dispoItems').findBy('id',id);
+    });
+  }),
+  includedItems_:[],
+
+});
+
+export default Route.extend({
+  model() {
+    return Listes.create({
+
+
+      dispoItems: [
+        {
+          "id": "1",
+          "url": "https://tutorialzine.com/2018/01/what-every-developer-should-know-about-cryptocurrency",
+          "title": "What Every Developer Should Know About Cryptocurrency",
+          "image": "https://tutorialzine.com/media/2018/01/everything-you-should-know-cryptocurrency.png"
+        },
+        {
+          "id": "2",
+          "url": "https://tutorialzine.com/2018/01/15-interesting-javascript-and-css-libraries-for-january-2018",
+          "title": "15 Interesting JavaScript and CSS Libraries for January 2018",
+          "image": "https://tutorialzine.com/media/2018/01/15-interesting-js-and-css-libraries-2018.png"
+        },
+        {
+          "id": "3",
+          "url": "https://tutorialzine.com/2017/12/the-best-javascript-and-css-libraries-for-2017",
+          "title": "The Best JavaScript and CSS Libraries for 2017",
+          "image": "https://tutorialzine.com/media/2017/12/the-best-javascript-and-css-libraries-for-2017.png"
+        },
+        {
+          "id": "4",
+          "url": "https://tutorialzine.com/2017/10/getting-started-with-graphql",
+          "title": "Getting Started With GraphQL ",
+          "image": "https://tutorialzine.com/media/2017/10/getting-started-graphql.png"
+        },
+        {
+          "id": "5",
+          "url": "https://tutorialzine.com/2017/11/9-free-icon-packs-for-web-developers",
+          "title": "9 Free Icon Packs For Web Developers",
+          "image": "https://tutorialzine.com/media/2017/10/9-free-icon-packs.png"
+        },
+        {
+          "id": "6", // testigherlkgjhrglkhdfgjkdhgjk:fhdglfdhgjk
+          "url": "https://tutorialzine.com/2018/01/10-free-programming-books-you-should-read-in-2018",
+          "title": "10 Free Programming Books You Should Read in 2018",
+          "image": "https://tutorialzine.com/media/2018/01/10-free-programming-books-you-should-read.png"
+        },
+        {
+          "id": "7",
+          "url": "https://tutorialzine.com/2016/12/the-languages-frameworks-tools-you-should-learn-in-2017",
+          "title": "The Languages and Frameworks You Should Learn in 2017",
+          "image": "https://tutorialzine.com/media/2016/12/the-languages-and-frameworks-you-should-learn-in-2017.png"
+        }
+      ]
+    });
+
+
+    /*actions: {
+      addToIncluded:function(model) {
+
+
+    },
+    clear:function(model){
+
+    },
+    getText:function(model){
+
+    }*/
+  }
+});
