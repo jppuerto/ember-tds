9b3e93b2ba8098ad547527a23f9fa8131978275a
Jeremy Puertolas <21600367@campus.unicaen.fr>
2018-02-14 14:40:01 +0100

    fin TD prix

--- servicesTD/app/routes/user.js
+++ servicesTD/app/routes/user.js
@@ -20,7 +20,23 @@ sumActive:Ember.computed('services.@each.active',function () {
       sum+=service.price;
   });
   return sum;
-})
+}),
+
+  remise : Ember.computed('codepromo','promos', function(){
+    return this.get('promos')[this.get('codepromo')] || '--';
+  }),
+
+  isRemise : Ember.computed('remise', function(){
+    return this.get('remise') > 0;
+  }),
+
+  sumRemise : Ember.computed('remise','sumActive', function(){
+    return this.get('sumActive')*this.get('remise');
+  }),
+
+  sumActiveRemise : Ember.computed('sumRemise','sumActive', function() {
+    return this.get('sumActive')-this.get('sumRemise');
+  })
 
 });
 
@@ -44,7 +60,15 @@ export default Route.extend({
       "name": "Formation",
         "price": 220,
         "active":false
-    }]
+
+    }],
+
+      promos:{
+          "B22":0.05,
+          "AZ":0.01,
+          "UBOAT":0.02
+      },
+      content:''
     });
   },
 
